2a545c80bc2c513e20ecab350a2787b2d5df95a2
olexanderf <110126454+olexanderf@users.noreply.github.com>
2022-09-06 21:34:14 +0300

    finish part two

--- part-two/js/main.js
+++ part-two/js/main.js
@@ -3,18 +3,35 @@ let textField = document.getElementById('text-field');
 let btnParse = document.getElementById('btn-parse');
 let resultField = document.getElementById('result-field');
 
-function parseCSV (text) {
-     text.split('\n')
-         .filter(part => part.charAt(0) !== "#")
-         .map((str)=>{
-         })
-        //  .forEach(element => {
-        //     console.log(element);
-        //  });
-    text
-    
+function parseCSV(textCsv) {
+    if (sessionStorage.getItem("cityRating") == undefined) {
+        let cityRating = {};
+        textCsv.split('\n')
+            .filter(part => part.match(/^[\d]+(?:\.\d{1,2})?,[\d]+(?:\.\d{1,2})?,[?!,.А-Яа-яёЁЇїІіЄєҐґЁ0-9\s]+,[\d]+[,]{0,}?$/))
+            .map(str => {
+                let arr = str.split(',')
+                return ({ x: arr[0], y: arr[1], name: arr[2], population: arr[3], })
+            })
+            .sort((a, b) => (b.population - a.population))
+            .slice(0, 10)
+            .reduce(((prev, el, index) => {
+                cityRating[el.name] = { population: el.population, rating: index + 1, }
+            }), {});
+        sessionStorage.setItem("cityRating", JSON.stringify(cityRating));
+        return replaceCity(textField.value, cityRating);
+    } 
+    return replaceCity(textField.value, JSON.parse(sessionStorage.getItem("cityRating")));
 }
 
-btnParse.addEventListener('click', ()=>{
+let replaceCity = (text, cityRating) => {
+    Object.keys(cityRating)
+        .forEach(city => {
+            text = text.replaceAll(city, `${city} (${cityRating[city].rating} место в ТОП-10 самых крупных городов Украины,
+                 население ${cityRating[city].population} человек)`)
+        });
+    resultField.textContent = text;
+}
+
+btnParse.addEventListener('click', () => {
     parseCSV(csvField.value);
 })
